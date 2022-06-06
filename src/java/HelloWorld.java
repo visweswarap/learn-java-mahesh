8f4d4c776209598cdaaa228e282a8114c311befb
Mahesh <mahesh.vonteru@gmail.com>
2022-06-06 10:20:55 +0530

    pushing initial change

--- src/java/HelloWorld.java
+++ src/java/HelloWorld.java
@@ -1,7 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int a = 10;
-
+        System.out.println("Name Number");
+        int a = 20;
+        if(a == 10)
+        {
+            System.out.println("Ten");
+        }
+        else if(a == 20 ){
+            System.out.println("Twenty");
+        }
+        else{
+            System.out.println("what you entered ");
+        }
     }
 }
