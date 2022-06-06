5faf4fe54ead8a85a682651e175df28e861808a9
Mahesh <mahesh.vonteru@gmail.com>
2022-06-06 11:13:37 +0530

    pushing initial change

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,6 +1,17 @@
-public class Helloworld
+ class Helloworld
 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 10;
+        if(a == 10)
+        {
+            system.out.println("Tem");
+        }
+        else-if(a == 20 ){
+            System.out.println("Twenty");
+        }
+        else{
+            System.out.println("what you entered ");
+        }
     }
 }
---
+++ src/java/Practice.java
@@ -0,0 +1,28 @@
+public class Practice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = b-a;
+        int d = a*b;
+        int e = b/a;
+        System.out.println("20-10 amount is "+ c);
+        System.out.println("addittion amount "+d);
+        System.out.println("Multiplication "+ d);
+        System.out.println("Division " + e);
+        if(a < b){
+            System.out.println("a is lesthan b");
+        }
+        else if(a > b){
+            System.out.println("a is grater than b");
+        }
+        else {
+             System.out.println("a is equals to b");
+            }
+        if(a == 10){
+            if(b == 20){
+                System.out.println("A = 10 AND b = 20");
+            }
+        }
+            }
+        }
+
