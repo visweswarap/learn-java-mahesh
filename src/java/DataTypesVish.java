e2d1d8d1654237e5471c7d76ae40770a40175d6d
Vish Pepala <vish@claz.org>
2022-06-07 09:26:18 +0530

    Added topics list Operators DataTypes

---
+++ src/java/DataTypesVish.java
@@ -0,0 +1,12 @@
+public class DataTypesVish {
+    public static void main(String[] args) {
+        String s = "wergwegr";
+        String s1 = new String("wergwegr");
+
+        if(s.equals(s1)){
+            System.out.println("matching.........");
+        } else {
+            System.out.println("Did not match....");
+        }
+    }
+}
---
+++ src/java/Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 34;
+        if (a == 10 && b== 10){
+            System.out.println("both same.........");
+        } if (a == 10 || b == 10) {
+            System.out.println("One record matched.....");
+        } else {
+            System.out.println("Nothing matched....");
+        }
+    }
+
+}
