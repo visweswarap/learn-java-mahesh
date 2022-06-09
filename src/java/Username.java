a1dbc20354e551fc1fa58633a9ea02e2162102b2
Vish Pepala <vish@claz.org>
2022-06-09 08:19:37 +0530

    cleanup

--- src/java/Username.java
+++ src/java/Username.java
@@ -6,19 +6,22 @@ public class Username {
         String username = user.nextLine();
         System.out.println("ENTER PASS WARD");
         String passward = user.nextLine();
-        if(!username.equals("mahesh")&&!passward.equals("vonteru@123")){
+        String usernameReq = "mahesh";
+        String passwordReq = "vonteru@123";
+
+        if(!username.equals(usernameReq)&&!passward.equals(passwordReq)){
             System.out.println("YOU ARE ENTER USER NAME OR PASSWARDS ARE NOT MATCHED TRY AGAIN");
             System.out.println("ENTER USER NAME");
             String username2 = user.nextLine();
             System.out.println("ENTER PASS WARD");
             String passward2 = user.nextLine();
-            if(!username2.equals("mahesh")&&!passward2.equals("vonteru@123")){
+            if(!username2.equals(usernameReq)&&!passward2.equals(passwordReq)){
                 System.out.println("YOU ARE ENTER USER NAME OR PASSWARDS ARE NOT MATCHED ONLY ONE CHANCE REMAINING  TRY AGAIN ");
                 System.out.println("ENTER USER NAME");
                 String username3 = user.nextLine();
                 System.out.println("ENTER PASS WARD");
                 String passward3 = user.nextLine();
-                if(!username3.equals("mahesh")&&!passward3.equals("vonteru@123")){
+                if(!username3.equals(usernameReq)&&!passward3.equals(passwordReq)){
                     System.out.println("YOU ARE ENTER USER NAME OR PASS WARDS ARE NOT MATCHED CONTACT NEAREST BRANCH  ");
                 }else{
                     System.out.println("CONGRATS !.... YOUR ACCOUNT BALANCE IS RS 10,000/-");}
